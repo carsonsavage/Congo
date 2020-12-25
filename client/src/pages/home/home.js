2735c43cb766713998d6bc915fc104188e121c09
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-24 23:10:24 -0600

    Create home.js

---
+++ client/src/pages/home/home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Home(){
+    return (
+        <>
+        <Header />
+        
+        <Wrapper>
+            <Featured />
+            <Statement />
+            <CallToAction />
+        </Wrapper>
+
+        <Footer />
+        </>
+    )
+};
+
+export default Home;
