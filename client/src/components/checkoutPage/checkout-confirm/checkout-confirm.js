7de658be41346a6eb39454e9b9f7a0924a3b4a68
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-26 00:01:53 -0600

    Create checkout-confirm.js

---
+++ client/src/components/checkoutPage/checkout-confirm/checkout-confirm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './checkout-confirm.css';
+
+export function CheckoutConfirmVertical(){
+    return (
+        <div className="checkout-vertical">
+            <h3>Vertical Checkout</h3>
+        </div>
+    )
+};
+
+export function CheckoutConfirmHorizontal(){
+    return (
+        <div className="checkout-horizontal">
+            <h3>Horizontal Checkout</h3>
+        </div>
+    )
+};
