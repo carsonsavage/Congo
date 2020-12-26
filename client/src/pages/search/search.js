2947f500ec940eb037353ee92a381a9979ae4489
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-25 22:12:32 -0600

    Update search.js

--- client/src/pages/search/search.js
+++ client/src/pages/search/search.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Header from '../../components/header/header.js';
+import Wrapper from '../../components/wrapper/wrapper.js';
+import SearchResults from '../../components/search-results/search-results.js';
+import Footer from '../../components/footer/footer.js';
 
 function Search(){
-    return <h2>Search</h2>
+    return (
+        <>
+        <Header />
+        <Wrapper>
+            <SearchResults />
+        </Wrapper>
+        <Footer />
+        </>
+    )
 };
 
 export default Search;
