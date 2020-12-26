38c8bb1fd73fc8ef1826a62f2ab3dc8915623a0f
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-25 23:18:45 -0600

    Update header.js

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './header.css';
 
-function Header(){
+function Header(props){
     return (
+        <>
+        {props.type === "checkout" ? 
+        <div className="header">
+            <h4>Checkout Header</h4>
+        </div>
+        :
         <div className="header">
             <h4>Header</h4>
         </div>
+        }
+        </>
+        
     )
 };
 
