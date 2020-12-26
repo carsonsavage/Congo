c8db7c5d6387a1142e35a7f09dda834b412422ed
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-25 22:12:30 -0600

    Update dashboard.js

--- client/src/pages/dashboard/dashboard.js
+++ client/src/pages/dashboard/dashboard.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { BrowserRouter as useParams } from "react-router-dom";
+import Footer from '../../components/footer/footer';
+import Header from '../../components/header/header';
+import Wrapper from '../../components/wrapper/wrapper';
+import AccountInfo from '../../components/account-info/account-info.js';
+import SavedAddress from '../../components/saved-address/saved-address.js';
+import SavedPayment from '../../components/saved-payment/saved-payment.js';
+import PrevOrders from '../../components/prev-orders/prev-orders.js';
 
 function UserDashboard(props){
-    return <h2>User: {props.match.params.id} Dashboard</h2>
+    console.log(props);
+
+    return (
+        <>
+        <Header />
+
+        <Wrapper>
+            <AccountInfo />
+            <SavedAddress />
+            <SavedPayment />
+            <PrevOrders />
+        </Wrapper>
+
+        <Footer />
+        </>
+    )
 };
 
 export default UserDashboard;
