f9c41ca5538599d272b4006bf4f6a284180f1e38
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-26 00:03:23 -0600

    Update home.js

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import Header from '../../components/header/header.js';
+import {MainHeader} from '../../components/header/header.js';
 import Wrapper from '../../components/wrapper/wrapper.js';
-import Featured from '../../components/featured/featured.js';
-import Statement from '../../components/statement/statement.js';
-import CallToAction from '../../components/call-to-action/call-to-action.js';
+import Featured from '../../components/homePage/featured/featured.js';
+import Statement from '../../components/homePage/statement/statement.js';
+import CallToAction from '../../components/homePage/call-to-action/call-to-action.js';
 import Footer from '../../components/footer/footer.js';
 
 function Home(){
     return (
         <div>
-            <Header />
+            <MainHeader />
             
             <Wrapper>
                 <Featured />
