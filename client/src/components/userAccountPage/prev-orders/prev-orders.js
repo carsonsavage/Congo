c943dcdd7bd0fe7421f9a3662670558c02880492
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-26 00:03:06 -0600

    Create prev-orders.js

---
+++ client/src/components/userAccountPage/prev-orders/prev-orders.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './prev-orders.css';
+
+function PrevOrders(){
+    return (
+        <div className="prev-orders">
+            <h3>Previous Orders</h3>
+        </div>
+    )
+};
+
+export default PrevOrders;
