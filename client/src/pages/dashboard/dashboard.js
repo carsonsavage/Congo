f04dcf8778b6a3a00eb2ab0b76d7847e8e9a1a4b
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-26 00:03:21 -0600

    Update dashboard.js

--- client/src/pages/dashboard/dashboard.js
+++ client/src/pages/dashboard/dashboard.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Footer from '../../components/footer/footer';
-import Header from '../../components/header/header';
+import {MainHeader} from '../../components/header/header';
 import Wrapper from '../../components/wrapper/wrapper';
-import AccountInfo from '../../components/account-info/account-info.js';
-import SavedAddress from '../../components/saved-address/saved-address.js';
-import SavedPayment from '../../components/saved-payment/saved-payment.js';
-import PrevOrders from '../../components/prev-orders/prev-orders.js';
+import AccountInfo from '../../components/userAccountPage/account-info/account-info.js';
+import SavedAddress from '../../components/userAccountPage/saved-address/saved-address.js';
+import SavedPayment from '../../components/userAccountPage/saved-payment/saved-payment.js';
+import PrevOrders from '../../components/userAccountPage/prev-orders/prev-orders.js';
 
 function UserDashboard(props){
     return (
         <>
-        <Header />
+        <MainHeader />
 
         <Wrapper>
             <AccountInfo id={props.match.params.id} />
