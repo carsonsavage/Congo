936a8e6f827edcec0ff3c14aab55a4b439b0aef7
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-26 00:02:04 -0600

    Update header.js

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import './header.css';
 
-function Header(props){
+export function MainHeader(){
     return (
-        <>
-        {props.type === "checkout" ? 
-        <div className="header">
-            <h4>Checkout Header</h4>
-        </div>
-        :
         <div className="header">
             <h4>Header</h4>
         </div>
-        }
-        </>
-        
     )
 };
 
-export default Header;
+export function CheckoutHeader(){
+    return (
+        <div className="header">
+            <h4>Checkout Header</h4>
+        </div>
+    )
+};
+
+export function LoginHeader(){
+    return (
+        <div className="header">
+            <h4>Login Header</h4>
+        </div>
+    )
+}
