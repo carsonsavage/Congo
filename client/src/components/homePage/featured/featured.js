27fba98f4013ffcc52d84c6d258e46a14bf826d2
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-26 00:02:11 -0600

    Create featured.js

---
+++ client/src/components/homePage/featured/featured.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './featured.css';
+
+function Featured(){
+    return (
+        <div className="featured">
+            <h3>featured</h3>
+        </div>
+    )
+};
+
+export default Featured;
