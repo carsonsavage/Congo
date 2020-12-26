e1be61a09f9766c17edf284cc23ba777f4c4b2a0
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-26 00:01:58 -0600

    Create checkout-details.js

---
+++ client/src/components/checkoutPage/checkout-details/checkout-details.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './checkout-details.css';
+
+function CheckoutDetails(){
+    return (
+        <div className="checkout-details">
+            <h3>Shipping</h3>
+            <hr />
+            <h3>Payment</h3>
+            <hr />
+            <h3>Items</h3>
+        </div>
+    )
+};
+
+export default CheckoutDetails;
