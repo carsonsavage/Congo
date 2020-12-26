b523e3784b9c7a6924658fbe2db8708dae806db5
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-25 22:12:27 -0600

    Create search-results.js

---
+++ client/src/components/search-results/search-results.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './search-results.css';
+
+
+function SearchResults(){
+    return (
+        <div className="search-results">
+            <h3>Search Results</h3>
+        </div>
+    )
+};
+
+export default SearchResults;
