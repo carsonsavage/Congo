a37a543dfbbae05ce7402d4f19fd63d0fc23946f
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-26 00:31:26 -0600

    Update orders.js

--- client/src/pages/orders/orders.js
+++ client/src/pages/orders/orders.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Footer from '../../components/footer/footer';
+import { MainHeader } from '../../components/header/header';
+import Wrapper from '../../components/wrapper/wrapper';
+import OrderSearch from '../../components/ordersPage/order-search/order-search.js';
+import OrderDisplay from '../../components/ordersPage/order-display/order-display.js';
 
 function Orders(){
-    return <h2>Orders</h2>
+    return (
+        <>
+        <MainHeader />
+
+        <Wrapper>
+            <OrderSearch />
+            <OrderDisplay />
+        </Wrapper>
+
+        <Footer />
+        </>
+    )
 };
 
 export default Orders;
