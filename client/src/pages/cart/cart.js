50802a19abb75d5ed472f71afa85ce8e1f157d1b
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-26 00:03:16 -0600

    Update cart.js

--- client/src/pages/cart/cart.js
+++ client/src/pages/cart/cart.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import {MainHeader} from '../../components/header/header.js';
+import Wrapper from '../../components/wrapper/wrapper.js';
+import CartCallToAction from '../../components/cartPage/cart-callToAction/cart-cta.js';
+import CartItems from '../../components/cartPage/cart-items/cart-items.js';
+import Footer from '../../components/footer/footer.js';
 
 function Cart(){
-    return <h2>Cart</h2>
+    return (
+        <>
+        <MainHeader />
+
+        <Wrapper>
+            <CartCallToAction />
+            <CartItems />
+        </Wrapper>
+
+        <Footer />
+        </>
+    )
 };
 
 export default Cart;
