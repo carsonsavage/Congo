ff26c0be8c98b4f7f1a224e1ac9c05e5da630479
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-26 00:31:11 -0600

    Create logo.js

---
+++ client/src/components/loginPage/logo/logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './logo.css';
+
+function Logo(){
+    return (
+        <div className="logo">
+            <h3>logo</h3>
+        </div>
+    )
+};
+
+export default Logo;
