378911428d93205cf61fdbaa95c02c19db080497
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-26 00:01:45 -0600

    Create cart-cta.js

---
+++ client/src/components/cartPage/cart-callToAction/cart-cta.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './cart-cta.css';
+
+function CartCallToAction(){
+    return (
+        <div className="cart-cta">
+            <h3>Cart Call to action</h3>
+        </div>
+    )
+};
+
+export default CartCallToAction;
