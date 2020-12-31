943634042e54500915e80273f557c73aa42604b9
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-12-31 17:12:55 -0600

    Update login.js

--- client/src/pages/login/login.js
+++ client/src/pages/login/login.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { LoginHeader } from '../../components/header/header';
 import Wrapper from '../../components/wrapper/wrapper';
 import Logo from '../../components/loginPage/logo/logo.js';
 import LoginForm from '../../components/loginPage/login-form/login-form.js';
@@ -8,7 +7,6 @@ import LoginBtn from '../../components/loginPage/login-btn/login-btn.js';
 function Login(){
     return (
         <>
-        <LoginHeader />
         <Wrapper>
             <Logo />
             <LoginForm />
