9230dcddb83ec2d4427be8b47b57c8556beafeb1
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-02 13:39:01 -0600

    Update account-info.js

--- client/src/components/userAccountPage/account-info/account-info.js
+++ client/src/components/userAccountPage/account-info/account-info.js
@@ -8,22 +8,53 @@ function AccountInfo() {
     const [editState, setEditState] = useState(false);
 
     function enableEdit() {
-        setEditState(true)
+        setEditState(true);
     }
 
-    function disableEdit() {
-        setEditState(false)
+    function saveEdit() {
+        setEditState(false);
     }
 
     return (
-        <div className="account-info">
-            <h3>Account Info</h3>
-            <hr />
+        <div class="ui raised segment">
+            <button class="ui red ribbon label">Overview</button>
+            <h4>Account Details</h4>
+            {editState ? (
+                <>
+                    <button className="circular ui icon button" onClick={saveEdit}>
+                        <i class="save icon"></i>
+                    </button>
+                    <hr />
+                    <div>
+                        <label>First Name: </label><br/>
+                        <input value={userState.first_name} onChange={handleUserInfoChange} name="first_name" type="text"/>
+                    </div>
+                    <div>
+                        <label>Last Name: </label><br/>
+                        <input value={userState.last_name} onChange={handleUserInfoChange} name="last_name" type="text"/>
+                    </div>
+                    <div>
+                        <label>Email: </label><br/>
+                        <input value={userState.email} onChange={handleUserInfoChange} name="email" type="email"/>
+                    </div>
 
-
-            <h5>last name: {userState.last_name}</h5>
-            <h5>email: {userState.email}</h5>
-            <h5>phone number: {userState.phone}</h5>
+                    <div>
+                        <label>Phone Number: </label><br/>
+                        <input value={userState.phone} onChange={handleUserInfoChange} name="phone" type="tel"/>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <button className="circular ui icon button" onClick={enableEdit}>
+                        <i class="edit icon"></i>
+                    </button>
+                    <hr />
+                    <p><span>First Name:</span> {userState.first_name}</p>
+                    <p><span>Last Name:</span> {userState.last_name}</p>
+                    <p><span>Email:</span> {userState.email}</p>
+                    <p><span>Phone Number:</span> {userState.phone}</p>
+                </>
+            )}
         </div>
     );
 }
