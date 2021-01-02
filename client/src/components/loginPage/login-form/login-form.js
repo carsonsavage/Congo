b84761854ab7907f27bc2cf3844f7eb5acc01352
Carson Savage <carsonesavage@gmail.com>
2021-01-02 10:34:54 -0600

    updated login and html and added animated logo

--- client/src/components/loginPage/login-form/login-form.js
+++ client/src/components/loginPage/login-form/login-form.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import './login-form.css';
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./login-form.css";
 
-function LoginForm(){
-    return (
-        <div className="login-form">
-            <h3>Login Form</h3>
-        </div>
-    )
-};
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default LoginForm;
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        
+      </Form>
+    </div>
+  );
+}
