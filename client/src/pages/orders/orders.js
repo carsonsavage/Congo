0e4aaf0365b1a20c8155fe70428d31b2a93c9241
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-03 13:56:12 -0600

    Update orders.js

--- client/src/pages/orders/orders.js
+++ client/src/pages/orders/orders.js
@@ -1,25 +1,38 @@
 import React from "react";
 import "./orders.css";
+import { Link } from "react-router-dom";
 import Footer from "../../components/footer/footer";
 import Wrapper from "../../components/wrapper/wrapper";
 import OrderSearch from "../../components/ordersPage/order-search/order-search.js";
-import OrderDisplay from "../../components/ordersPage/order-display/order-display.js";
+import OrderDisplay from "../../components/ordersPage/prev-orders/prev-orders.js";
 
 function Orders() {
     return (
         <>
             <Wrapper>
                 <div className="orders-header">
-                    <h1 class="ui icon header">
-                        <i class="massive box icon"></i>
-                        <div class="content">
-                            Orders
-                        </div>
+                    <h1 className="ui icon header">
+                        <i className="massive box icon"></i>
+                        <div className="content">Orders</div>
                     </h1>
                 </div>
                 <hr />
-                <OrderSearch />
-                <OrderDisplay />
+                {false ? (
+                    <>
+                        <OrderSearch />
+                        <OrderDisplay />
+                    </>
+                ) : (
+                    <div className="ui placeholder segment">
+                        <div className="ui icon header">
+                            <i className="users icon"></i>
+                            You need to login to see previous orders
+                        </div>
+                        <Link to="/login" className="react-link">
+                            <div className="ui green huge button">Login</div>
+                        </Link>
+                    </div>
+                )}
             </Wrapper>
 
             <Footer />
