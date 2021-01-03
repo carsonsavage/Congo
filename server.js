5249c9a9f6bd7ab05d29d93f80d056e568ce029c
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-03 14:07:05 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+//setting up node env variable reading
+const express = require("express");
+require("dotenv").config();
+
+//getting express server
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+
+// Need to add routes here
+
+
+// Connect to the Mongo DB
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/congo", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+// Start the API server
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
