75b29a8e9d0d4964f8275e4a24ce008e6860042f
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-02 18:57:03 -0600

    Update checkout.js

--- client/src/pages/checkout/checkout.js
+++ client/src/pages/checkout/checkout.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import './checkout.css';
 import Wrapper from '../../components/wrapper/wrapper.js';
-import {CheckoutConfirmHorizontal, CheckoutConfirmVertical} from '../../components/checkoutPage/checkout-confirm/checkout-confirm.js';
+import { CheckoutConfirmHorizontal, CheckoutConfirmVertical } from '../../components/checkoutPage/checkout-confirm/checkout-confirm.js';
 import CheckoutDetails from '../../components/checkoutPage/checkout-details/checkout-details.js';
 
 
-function Checkout(){
+function Checkout() {
     return (
         <>
 
-        <Wrapper>
-            <CheckoutConfirmVertical />
-            <CheckoutDetails />
-            <CheckoutConfirmHorizontal />
-        </Wrapper>
+            <Wrapper>
+                <div className="clearfix">
+                    <CheckoutDetails />
+                    <CheckoutConfirmVertical />
+                    <CheckoutConfirmHorizontal />
+                </div>
+
+            </Wrapper>
         </>
     )
 };
