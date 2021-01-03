1ad855d993997347dcbd5a019df184c0d936b197
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-02 18:56:58 -0600

    Update prev-orders.js

--- client/src/components/userAccountPage/prev-orders/prev-orders.js
+++ client/src/components/userAccountPage/prev-orders/prev-orders.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './prev-orders.css';
+import UserContext from "../../../util/userContext.js";
+import OrderCards from '../order-cards/order-cards.js';
 
 function PrevOrders(){
+    const { userState, handleUserInfoChange } = useContext(UserContext);
+
     return (
         <div className="ui raised segment">
             <button className="ui orange ribbon label">Orders</button>
             <h4>Previous Orders</h4>
-            
             <hr />
+            <div class="ui stacked segments">
+                <OrderCards />
+            </div>
         </div>
     )
 };
