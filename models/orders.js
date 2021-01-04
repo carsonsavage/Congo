895c2772ca5acc5d116fffdeaf2b8ecfcfd2cc91
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-04 17:55:32 -0600

    create and design signup form

--- models/orders.js
+++ models/orders.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-    order_id: String,
+    order_num: String,
     user_id: String,
-    products: Array,
+    items: Array,
+    order_total: Number,
     total: Number,
     order_date: { type: Date, default: Date.now }
 });
