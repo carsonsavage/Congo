48499e137751ec16bb1ef9f65946ae5c4c23125e
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-04 22:01:55 -0600

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,12 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const db = require("../models");
 
-function isValidPassword(dbpassword, password){
-    bcrypt.compare(password, dbpassword, (err, result)=>{
-        console.log(result);
+function isValidPassword(dbpassword, password) {
+    bcrypt.compare(password, dbpassword, (err, result) => {
+        return result
     });
 };
 
@@ -19,26 +19,27 @@ passport.use(
         },
         function (email, password, done) {
             // When a user tries to sign in this code runs
-            db.User.findOne({
-                where: {
+            db.User.findOne(
+                {
                     email: email,
                 },
-            }).then(function (dbUser) {
-                // If there's no user with the given email
-                if (!dbUser) {
-                    return done(null, false, {
-                        message: "Incorrect email.",
-                    });
+                (err, dbUser) => {
+                    // If there's no user with the given email
+                    if (!dbUser) {
+                        return done(null, false, {
+                            message: "Incorrect email.",
+                        });
+                    }
+                    // If there is a user with the given email, but the password the user gives us is incorrect
+                    else if (isValidPassword(dbUser.password, password)) {
+                        return done(null, false, {
+                            message: "Incorrect password.",
+                        });
+                    }
+                    // If none of the above, return the user
+                    return done(null, dbUser);
                 }
-                // If there is a user with the given email, but the password the user gives us is incorrect
-                else if (!isValidPassword(dbUser.password, password)) {
-                    return done(null, false, {
-                        message: "Incorrect password.",
-                    });
-                }
-                // If none of the above, return the user
-                return done(null, dbUser);
-            });
+            );
         }
     )
 );
