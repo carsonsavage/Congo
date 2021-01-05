267d9c70388a788852f73e94d3744d570e0d9df3
Carson Savage <carsonesavage@gmail.com>
2021-01-04 21:41:04 -0600

    adding carousel to main page, needing to edit

--- client/src/components/homePage/statement/statement.js
+++ client/src/components/homePage/statement/statement.js
@@ -3,10 +3,28 @@ import './statement.css';
 
 function Statement(){
     return (
-        <div className="statement">
-            <h3>statement</h3>
-        </div>
-    )
-};
+    <div id="carouselslides" className="carousel slide" data-ride="carousel">
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <img className="d-block w-100" src="https://i.imgur.com/5JId3WT.png" alt="First slide" />
+      </div>
+      <div className="carousel-item2">
+        <img className="d-block w-100" src="https://i.imgur.com/LPDTa02.png" alt="Second slide" />
+      </div>
+      <div className="carousel-item3">
+        <img className="d-block w-100" src="https://i.imgur.com/IDumiJ3.png" alt="Third slide" />
+      </div>
+    </div>
+    <a className="carousel-control-prev" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
+          <span className="sr-only">Previous</span>
+        </a>
+        <a className="carousel-control-next" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true" />
+          <span className="sr-only">Next</span>
+        </a>
+  </div>
+);
+}
 
 export default Statement;
