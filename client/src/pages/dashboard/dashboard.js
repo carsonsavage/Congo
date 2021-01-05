66673c285073b994211c28a7ed724d3c3cfe5680
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-05 15:03:59 -0600

    Update dashboard.js

--- client/src/pages/dashboard/dashboard.js
+++ client/src/pages/dashboard/dashboard.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import './dashboard.css';
-import Footer from '../../components/footer/footer';
-import Wrapper from '../../components/wrapper/wrapper';
-import AccountInfo from '../../components/userAccountPage/account-info/account-info.js';
-import SavedAddress from '../../components/userAccountPage/saved-address/saved-address.js';
-import SavedPayment from '../../components/userAccountPage/saved-payment/saved-payment.js';
+import React, { useContext } from "react";
+import "./dashboard.css";
+import Footer from "../../components/footer/footer";
+import Wrapper from "../../components/wrapper/wrapper";
+import AccountInfo from "../../components/userAccountPage/account-info/account-info.js";
+import SavedAddress from "../../components/userAccountPage/saved-address/saved-address.js";
+import SavedPayment from "../../components/userAccountPage/saved-payment/saved-payment.js";
+import UserContext from "../../util/userContext.js";
 
 function UserDashboard() {
-
+    const { logoutUser } = useContext(UserContext);
     return (
         <>
-
             <Wrapper>
                 <div className="dashboard-div">
+                    <button onClick={logoutUser}>Logout</button>
                     <AccountInfo />
                     <SavedAddress />
                     <SavedPayment />
@@ -21,7 +22,7 @@ function UserDashboard() {
 
             <Footer />
         </>
-    )
-};
+    );
+}
 
 export default UserDashboard;
