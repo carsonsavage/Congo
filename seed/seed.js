f4c3a17c7eae7c317d480a7bf2f81a599fe414f4
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-05 15:23:04 -0600

    Update seed.js

--- seed/seed.js
+++ seed/seed.js
@@ -1,21 +1,20 @@
 const mongoose = require("mongoose");
 const db = require("../models");
-const productsSeed = require('./products.json');
+const productsSeed = require("./products.json");
 
-mongoose.connect("mongodb://localhost/congo", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/congo", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
 });
 
-
-
 db.Product.deleteMany({})
-  .then(() => db.Product.collection.insertMany(productsSeed))
-  .then(data => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+    .then(() => db.Product.collection.insertMany(productsSeed))
+    .then((data) => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0);
+    })
+    .catch((err) => {
+        console.error(err);
+        process.exit(1);
+    });
