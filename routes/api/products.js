47246d6a34eb52efc6f29542895b3ab2d2d995ae
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-05 17:26:24 -0600

    Update products.js

--- routes/api/products.js
+++ routes/api/products.js
@@ -3,42 +3,42 @@ const productsController = require("../../controllers/productsController");
 
 // Matches with "/api/products/search"
 router.route("/search").post(({ body }, res) => {
-  const {category, query} = body;
+    const { category, query } = body;
     if (category && query) {
         productsController
             .findByCategoryAndQuery(category, query)
             .then((data) => {
-                console.log(data.length);
+                res.json(data);
             })
-            .catch((error) => {
-                console.log(error);
+            .catch((err) => {
+                res.status(422).json(err);
             });
     } else if (category) {
         productsController
             .findByCategory(category)
             .then((data) => {
-                console.log(data.length);
+                res.json(data);
             })
-            .catch((error) => {
-                console.log(error);
+            .catch((err) => {
+                res.status(422).json(err);
             });
     } else if (query) {
         productsController
             .findByQuery(query)
             .then((data) => {
-                console.log(data.length);
+                res.json(data);
             })
-            .catch((error) => {
-              res.status(422).json(err);
+            .catch((err) => {
+                res.status(422).json(err);
             });
     } else {
         productsController
             .findAll()
             .then((data) => {
-                console.log(data.length);
+                res.json(data);
             })
-            .catch((error) => {
-              res.status(422).json(err);
+            .catch((err) => {
+                res.status(422).json(err);
             });
     }
 });
