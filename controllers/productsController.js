592a643986429484073ba8cceec5d43bcf06d1fd
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-05 17:25:26 -0600

    Update productsController.js

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,31 +2,32 @@ const db = require("../models");
 
 // Defining methods for the productsController
 module.exports = {
-  findAll: function(req, res) {
-    db.Product.find(req.query)
-      .sort({ keywords: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  findById: function(req, res) {
-    db.Product.findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  create: function(req, res) {
-    db.Product.create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  update: function(req, res) {
-    db.Product.findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  remove: function(req, res) {
-    db.Product.findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  }
+    findAll: function () {
+        return db.Product.find({});
+    },
+    findByCategory: function (category) {
+        return db.Product.find({ category: category });
+    },
+    findByQuery: function (query) {
+        return db.Product.find({ $text: { $search: `"\"${query}\""` } });
+    },
+    findByCategoryAndQuery: function (category, query) {
+        return db.Product.find({ category: category, $text: { $search: query } });
+    },
+    create: function (req, res) {
+        db.Product.create(req.body)
+            .then((dbModel) => res.json(dbModel))
+            .catch((err) => res.status(422).json(err));
+    },
+    update: function (req, res) {
+        db.Product.findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then((dbModel) => res.json(dbModel))
+            .catch((err) => res.status(422).json(err));
+    },
+    remove: function (req, res) {
+        db.Product.findById({ _id: req.params.id })
+            .then((dbModel) => dbModel.remove())
+            .then((dbModel) => res.json(dbModel))
+            .catch((err) => res.status(422).json(err));
+    },
 };
