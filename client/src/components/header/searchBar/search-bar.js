d5cfbed71f53f0058a361fbb58e12d0657059bae
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-05 17:25:23 -0600

    Update search-bar.js

--- client/src/components/header/searchBar/search-bar.js
+++ client/src/components/header/searchBar/search-bar.js
@@ -3,8 +3,22 @@ import { Link } from "react-router-dom";
 import "./search-bar.css";
 import SearchContext from "../../../util/searchContext";
 
+
+
 function SearchBar() {
-    const { searchState, handleSearchChange } = useContext(SearchContext);
+    const { searchState, handleSearchChange, searchProducts } = useContext(SearchContext);
+
+    function generateSearchQuery() {
+        let query = `/search/`;
+        if(searchState.search_category){
+            query += `category=${searchState.search_category}&`
+        };
+        if(searchState.search_query){
+            query += `query=${searchState.search_query}`
+        };
+        
+        return query;
+    }
 
     return (
         <div className="search-bar">
@@ -18,9 +32,10 @@ function SearchBar() {
             </div>
 
             <Link
-                to={`/search/${searchState.search_query}`}
+                to={generateSearchQuery()}
                 role="button"
                 className="ui inverted blue button"
+                onClick={searchProducts}
             >
                 <i className="search icon" />
             </Link>
