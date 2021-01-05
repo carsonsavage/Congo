62fe61c65c2de6e456860875ed02a9c17552f29c
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-05 17:25:28 -0600

    Update products.js

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,17 +1,46 @@
 const router = require("express").Router();
 const productsController = require("../../controllers/productsController");
 
-// Matches with "/api/posts"
-router
-  .route("/")
-  .get(productsController.findAll)
-  .post(productsController.create);
-
-// Matches with "/api/posts/:id"
-router
-  .route("/:id")
-  .get(productsController.findById)
-  .put(productsController.update)
-  .delete(productsController.remove);
+// Matches with "/api/products/search"
+router.route("/search").post(({ body }, res) => {
+  const {category, query} = body;
+    if (category && query) {
+        productsController
+            .findByCategoryAndQuery(category, query)
+            .then((data) => {
+                console.log(data.length);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    } else if (category) {
+        productsController
+            .findByCategory(category)
+            .then((data) => {
+                console.log(data.length);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    } else if (query) {
+        productsController
+            .findByQuery(query)
+            .then((data) => {
+                console.log(data.length);
+            })
+            .catch((error) => {
+              res.status(422).json(err);
+            });
+    } else {
+        productsController
+            .findAll()
+            .then((data) => {
+                console.log(data.length);
+            })
+            .catch((error) => {
+              res.status(422).json(err);
+            });
+    }
+});
 
 module.exports = router;
