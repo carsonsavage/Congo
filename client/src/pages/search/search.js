b0546d388fb1b0e32670ff620aeb9076ed49c149
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-05 18:25:56 -0600

    Update search.js

--- client/src/pages/search/search.js
+++ client/src/pages/search/search.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Wrapper from '../../components/wrapper/wrapper.js';
-import SearchResults from '../../components/search-results/search-results.js';
+import SearchResults from '../../components/searchResultsPage/search-results.js';
 import Footer from '../../components/footer/footer.js';
 
 function Search(){
