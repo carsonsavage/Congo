a5580dab1abb65a6296311f7a212112532d0c3ce
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-06 15:23:08 -0600

    updating search query and page reload persist

--- client/src/pages/search/search.js
+++ client/src/pages/search/search.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import Wrapper from '../../components/wrapper/wrapper.js';
-import SearchResults from '../../components/searchResultsPage/search-results.js';
-import Footer from '../../components/footer/footer.js';
+import React, { useEffect, useContext } from "react";
+import Wrapper from "../../components/wrapper/wrapper.js";
+import SearchResults from "../../components/searchResultsPage/search-results.js";
+import Footer from "../../components/footer/footer.js";
+import SearchContext from "../../util/searchContext.js";
 
-function Search(){
+function Search(props) {
+    const { searchState, searchProducts } = useContext(SearchContext);
+
+    const { params } = props.match;
+
+    useEffect(() => {
+        if (searchState.search_category || searchState.search_query) {
+            console.log("do nothing");
+        } else {
+            searchProducts(params.category, params.query);
+        }
+    }, []);
+
     return (
         <>
-        <Wrapper>
-            <SearchResults />
-        </Wrapper>
-        <Footer />
+            <Wrapper>
+                <SearchResults />
+            </Wrapper>
+            <Footer />
         </>
-    )
-};
+    );
+}
 
 export default Search;
