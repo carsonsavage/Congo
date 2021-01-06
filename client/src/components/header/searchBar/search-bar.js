8433400c3f722d0160606a71758a79d66ee7531e
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-06 15:23:04 -0600

    updating search query and page reload persist

--- client/src/components/header/searchBar/search-bar.js
+++ client/src/components/header/searchBar/search-bar.js
@@ -1,44 +1,51 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "./search-bar.css";
 import SearchContext from "../../../util/searchContext";
 
-
-
 function SearchBar() {
-    const { searchState, handleSearchChange, searchProducts } = useContext(SearchContext);
+    const history = useHistory();
+
+    const { searchState, handleSearchChange, searchProducts } = useContext(
+        SearchContext
+    );
 
     function generateSearchQuery() {
-        let query = `/search/`;
-        if(searchState.search_category){
-            query += `category=${searchState.search_category}&`
-        };
-        if(searchState.search_query){
-            query += `query=${searchState.search_query}`
-        };
-        
+        let query = `/search/C=${searchState.search_category}&Q=${searchState.search_query}`;
+
         return query;
     }
 
     return (
         <div className="search-bar">
-            <div className="ui input">
-                <input
-                    name="search"
-                    type="text"
-                    onChange={handleSearchChange}
-                    value={searchState.search_query}
-                />
-            </div>
-
-            <Link
-                to={generateSearchQuery()}
-                role="button"
-                className="ui inverted blue button"
-                onClick={searchProducts}
+            <form
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    history.push(generateSearchQuery());
+                    searchProducts(
+                        searchState.search_category,
+                        searchState.search_query
+                    );
+                }}
             >
-                <i className="search icon" />
-            </Link>
+                <div className="ui input">
+                    <input
+                        name="search"
+                        type="text"
+                        onChange={handleSearchChange}
+                        value={searchState.search_query}
+                    />
+                </div>
+
+                <Link
+                    to={generateSearchQuery()}
+                    role="button"
+                    type="submit"
+                    className="ui inverted blue button"
+                >
+                    <i className="search icon" />
+                </Link>
+            </form>
         </div>
     );
 }
