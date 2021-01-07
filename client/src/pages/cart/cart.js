ea3fec4a4b3d7a3e0e76965269bb693f2c35aa36
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-07 17:16:24 -0600

    Update cart.js

--- client/src/pages/cart/cart.js
+++ client/src/pages/cart/cart.js
@@ -1,18 +1,36 @@
-import React from "react";
-import './cart.css';
+import React, { useContext } from "react";
+import "./cart.css";
 import Wrapper from "../../components/wrapper/wrapper.js";
 import CartCallToAction from "../../components/cartPage/cart-callToAction/cart-cta.js";
 import CartItems from "../../components/cartPage/cart-items/cart-items.js";
 import Footer from "../../components/footer/footer.js";
 import { Row, Col } from "react-bootstrap";
+import CartContext from "../../util/cartContext.js";
+import { Link } from "react-router-dom";
 
 function Cart() {
+    const { cartState } = useContext(CartContext);
+
     return (
         <>
             <Wrapper>
                 <Row className="cart-page">
                     <Col md={9}>
-                        <CartItems />
+                        {cartState.cart_item_count > 0 ? (
+                            <CartItems />
+                        ) : (
+                            <div className="ui placeholder segment">
+                                <div className="ui icon header">
+                                    <i className="cart icon"></i>
+                                    Your cart is currently empty
+                                </div>
+                                <Link to="/" className="react-link">
+                                    <div className="ui blue mini button">
+                                        Continue Shopping
+                                    </div>
+                                </Link>
+                            </div>
+                        )}
                     </Col>
                     <Col md={3}>
                         <CartCallToAction />
