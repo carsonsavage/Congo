eb9296ad4dad80f5283b0576575f9f717ce3ecc9
Carson Savage <carsonesavage@gmail.com>
2021-01-06 21:02:50 -0600

    updated carousel width, adding cards for featured products

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -13,13 +13,6 @@ function Header() {
         <Navbar bg="dark" expand="lg" className="header">
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Brand href="#home" className="brand">
-                <img
-                    alt=""
-                    src="https://imgur.com/sKQZp7j"
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                />{" "}
                 <Link to="/" className="react-link">
                     Congo
                 </Link>
--- client/src/components/homePage/featured/featured.js
+++ client/src/components/homePage/featured/featured.js
@@ -1,12 +1,27 @@
-import React from 'react';
 import './featured.css';
-
-function Featured(){
-    return (
-        <div className="featured">
-            <h3>featured</h3>
-        </div>
-    )
+import React, { useState } from 'react';
+import ItemsCarousel from 'react-items-carousel';
+ 
+export default () => {
+  const [activeItemIndex, setActiveItemIndex] = useState(0);
+  const chevronWidth = 30;
+  return (
+    <div style={{ padding: `0 ${chevronWidth}px` }}>
+      <ItemsCarousel
+        requestToChangeActive={setActiveItemIndex}
+        activeItemIndex={activeItemIndex}
+        numberOfCards={3}
+        gutter={30}
+        leftChevron={<button>{'<'}</button>}
+        rightChevron={<button>{'>'}</button>}
+        outsideChevron
+        chevronWidth={chevronWidth}
+      >
+        <div style={{ height: 300, background: '#EEE' }}>First card</div>
+        <div style={{ height: 300, background: '#EEE' }}>Second card</div>
+        <div style={{ height: 300, background: '#EEE' }}>Third card</div>
+        <div style={{ height: 300, background: '#EEE' }}>Fourth card</div>
+      </ItemsCarousel>
+    </div>
+  );
 };
-
-export default Featured;
