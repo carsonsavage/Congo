537a5c3d0af0db351482522ea49e0bf021b99800
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-06 21:45:22 -0600

    Update product-details.js

--- client/src/pages/product-details/product-details.js
+++ client/src/pages/product-details/product-details.js
@@ -6,42 +6,22 @@ import { Col, Row, Image } from "react-bootstrap";
 import { Rating } from "semantic-ui-react";
 import Wrapper from "../../components/wrapper/wrapper";
 import Footer from "../../components/footer/footer";
-import SmallProductImages from "../../components/productDetailsPage/small-product-images/small-product-images.js";
 import AboutItemList from "../../components/productDetailsPage/product-info/about-item.js";
 import ItemDescription from "../../components/productDetailsPage/product-info/item-description.js";
 import ProductCta from "../../components/productDetailsPage/cta/product-details-cta.js";
-
+import ImageState from "./imageState.js";
 function Orders(props) {
     const { params } = props.match;
     const history = useHistory();
 
-    const sampleImageArray = [
-        "https://images-na.ssl-images-amazon.com/images/I/71nhQn8zoUL._SL1500_.jpg",
-        "https://images-na.ssl-images-amazon.com/images/I/71Y7-ZBt24L._SL1240_.jpg",
-        "https://images-na.ssl-images-amazon.com/images/I/71yBHHy5hXL._SL1500_.jpg",
-    ];
-
     const ratingValue = parseInt(Math.floor(Math.random() * 5));
-    //set original image here
-    const [imgState, setImageState] = useState(sampleImageArray[0]);
+    const ratingCount = parseInt(Math.floor(Math.random() * 2946));
 
     return (
         <>
             <Wrapper>
                 <Row className="product-details">
-                    <Col md={1}>
-                        <div className="small-images">
-                            <SmallProductImages
-                                images={sampleImageArray}
-                                setImageState={setImageState}
-                            />
-                        </div>
-                    </Col>
-                    <Col md={5}>
-                        <div className="image">
-                            <Image src={imgState} fluid />
-                        </div>
-                    </Col>
+                    <ImageState />
                     <Col md={4}>
                         <div className="title">
                             <h2>
@@ -59,7 +39,7 @@ function Orders(props) {
                                 {" "}
                                 |{" "}
                                 <NumberFormat
-                                    value={2832}
+                                    value={ratingCount}
                                     displayType={"text"}
                                     thousandSeparator={true}
                                 />{" "}
@@ -87,7 +67,7 @@ function Orders(props) {
                     </Col>
                 </Row>
                 <Row>
-                    <Col>
+                    <Col id="description">
                         <hr />
                         <h4>Description</h4>
                         <ItemDescription description="lalalalllllllllllllllllllllllllll" />
