4ba7ec1dac80e1a1e480af18100eb9f0177112c9
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-07 14:18:11 -0600

    Update imageState.js

--- client/src/pages/product-details/imageState.js
+++ client/src/pages/product-details/imageState.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Image } from "react-bootstrap";
 import SmallProductImages from "../../components/productDetailsPage/small-product-images/small-product-images.js";
 
 function ImageState({images}) {
 
-    const [imgState, setImageState] = useState(images[0]);
+    const [imgState, setImageState] = useState();
+
+    useEffect(()=>{
+        setImageState(images[0])
+    }, [images])
 
     return (
         <>
