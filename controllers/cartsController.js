72808a1827bea830f1e7726b2b888b380d1fccf1
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-07 17:41:08 -0600

    Create cartsController.js

---
+++ controllers/cartsController.js
@@ -0,0 +1,15 @@
+const db = require("../models");
+
+// Defining methods for the usersController
+module.exports = {
+    findById: function (req, res) {
+        db.User.findById(req.params.id)
+            .then((dbModel) => res.json(dbModel))
+            .catch((err) => res.status(422).json(err));
+    },
+    update: function (req, res) {
+        db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then((dbModel) => res.json(dbModel))
+            .catch((err) => res.status(422).json(err));
+    }
+};
