f6caa3412d702a11efc96785d071bb761066ce73
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-07 23:44:59 -0600

    Update cartsController.js

--- controllers/cartsController.js
+++ controllers/cartsController.js
@@ -3,13 +3,21 @@ const db = require("../models");
 // Defining methods for the usersController
 module.exports = {
     findById: function (req, res) {
-        db.User.findById(req.params.id)
+        db.Cart.findById(req.params.id)
             .then((dbModel) => res.json(dbModel))
             .catch((err) => res.status(422).json(err));
     },
     update: function (req, res) {
-        db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
+        console.log("here");
+        db.Cart.findOneAndUpdate(
+            { _id: req.params.id },
+            { user_id: req.params.id, cart_items: req.body },
+            {
+                returnNewDocument: true,
+                upsert: true,
+            }
+        )
             .then((dbModel) => res.json(dbModel))
             .catch((err) => res.status(422).json(err));
-    }
+    },
 };
