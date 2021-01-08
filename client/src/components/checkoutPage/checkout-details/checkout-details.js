3f10a23dca376f70a4c0b0c335d56c5617ed3576
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-07 23:44:54 -0600

    Update checkout-details.js

--- client/src/components/checkoutPage/checkout-details/checkout-details.js
+++ client/src/components/checkoutPage/checkout-details/checkout-details.js
@@ -1,27 +1,82 @@
-import React, { useState } from 'react';
-import './checkout-details.css';
-
-function Shipping(){
-    return <h2>Shipping</h2>
-};
-
-function Billing(){
-    return <h2>Billing</h2>
-};
-
-function Confirm(){
-    return <h2>Confirm</h2>
-};
-
-
-
-
+import React, { useContext, useState } from "react";
+import UserContext from "../../../util/userContext";
+import "./checkout-details.css";
 
 function CheckoutDetails() {
-    const [displayState, setDisplayState] = useState();
+    const {userState} = useContext(UserContext);
     const [shippingState, setShippingState] = useState("active");
     const [paymentState, setPaymentState] = useState("disabled");
     const [confirmState, setConfirmState] = useState("disabled");
+
+    console.log(userState.address)
+
+    function Shipping() {
+        return (
+            <>
+                <h2>Shipping</h2>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setShippingState("completed");
+                        setPaymentState("active");
+                    }}
+                >
+                    Confirm
+                </button>
+            </>
+        );
+    }
+
+    function Billing() {
+        return (
+            <>
+                <h2>Billing</h2>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setPaymentState("completed");
+                        setConfirmState("active");
+                    }}
+                >
+                    Confirm
+                </button>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setPaymentState("disabled")
+                        setShippingState("active");
+                    }}
+                >
+                    Go Back
+                </button>
+            </>
+        );
+    }
+
+    function Confirm() {
+        return (
+            <>
+                <h2>Confirm Order</h2>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setConfirmState("completed");
+                    }}
+                >
+                    Confirm
+                </button>
+                <button
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setPaymentState("active")
+                        setConfirmState("disabled");
+                    }}
+                >
+                    Go Back
+                </button>
+            </>
+        );
+    }
 
     return (
         <div className="checkout-details">
@@ -30,17 +85,19 @@ function CheckoutDetails() {
                     <i class="truck icon"></i>
                     <div class="content">
                         <div class="title">Shipping</div>
-                        <div class="description">Choose your shipping options</div>
+                        <div class="description">
+                            Choose your shipping options
+                        </div>
                     </div>
                 </div>
-                <div class="diabled step">
+                <div class={`step ${paymentState}`}>
                     <i class="payment icon"></i>
                     <div class="content">
                         <div class="title">Billing</div>
                         <div class="description">Enter billing information</div>
                     </div>
                 </div>
-                <div class="disabled step">
+                <div class={`step ${confirmState}`}>
                     <i class="info icon"></i>
                     <div class="content">
                         <div class="title">Confirm Order</div>
@@ -49,10 +106,12 @@ function CheckoutDetails() {
                 </div>
             </div>
             <div class="ui attached segment">
-                {}
+                {shippingState === "active" && Shipping()}
+                {paymentState === "active" && Billing()}
+                {confirmState === "active" && Confirm()}
             </div>
         </div>
-    )
-};
+    );
+}
 
 export default CheckoutDetails;
