49cbe29bd9592e90ec5a9e44fac7d1a0e6e9d925
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-07 20:03:06 -0600

    Update cart-cards.js

--- client/src/components/cartPage/cart-cards/cart-cards.js
+++ client/src/components/cartPage/cart-cards/cart-cards.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./cart-cards.css";
 import { Segment, Button } from "semantic-ui-react";
 import { Row, Col, Image, Form } from "react-bootstrap";
+import CartContext from "../../../util/cartContext.js";
 
 function CartCards() {
+    const { cartState } = useContext(CartContext);
+
     function generateOptions(count) {
         let i;
         let genQty = [];
@@ -12,8 +15,8 @@ function CartCards() {
         }
         return (
             <>
-                {genQty.map((num) => (
-                    <option>{num}</option>
+                {genQty.map((num, index) => (
+                    <option key={index}>{num}</option>
                 ))}
             </>
         );
@@ -21,48 +24,48 @@ function CartCards() {
 
     return (
         <>
-            <Segment padded>
-                <Row>
-                    <Col md={2}>
-                        <Image
-                            src="https://images-na.ssl-images-amazon.com/images/I/61CAmyJBxBL._AC_SL1000_.jpg"
-                            thumbnail
-                            className="card-cards"
-                        />
-                    </Col>
-                    <Col md={8}>
-                        <h5>
-                            IHEIPYE Grandma Sonogram Picture Frame - Baby
-                            Announcement Gifts Grandparents Frame - Grandma
-                            Pregnancy Announcement Gift, Soon To Be Grandma
-                            Gifts, Hello Grandma See You Soon, Rustic Natural
-                        </h5>
-                        <div className="clearfix cta-divs">
-                            <label className="float-left">Qty:</label>
-                            <Form.Control
-                                as="select"
-                                size="sm"
-                                className="select float-left"
-                            >
-                                {generateOptions(13)}
-                            </Form.Control>
-                        </div>
-                        <div>
-                            <Button
-                                content="Delete"
-                                icon="delete"
-                                labelPosition="left"
-                                className="mini deleteBtn red"
-                            />
-                        </div>
-                    </Col>
-                    <Col md={2}>
-                        <h5 className="float-right">
-                            <span>${12.32}</span>
-                        </h5>
-                    </Col>
-                </Row>
-            </Segment>
+            {cartState.cart_items.map(
+                ({ _id, images, title, quantity, price }, index) => (
+                    <Segment padded key={index}>
+                        <Row>
+                            <Col md={2}>
+                                <Image
+                                    src={images[0]}
+                                    thumbnail
+                                    className="card-cards"
+                                    alt={title}
+                                />
+                            </Col>
+                            <Col md={8}>
+                                <h5>{title}</h5>
+                                <div className="clearfix cta-divs">
+                                    <label className="float-left">Qty:</label>
+                                    <Form.Control
+                                        as="select"
+                                        size="sm"
+                                        className="select float-left"
+                                    >
+                                        {generateOptions(quantity)}
+                                    </Form.Control>
+                                </div>
+                                <div>
+                                    <Button
+                                        content="Delete"
+                                        icon="delete"
+                                        labelPosition="left"
+                                        className="mini deleteBtn red"
+                                    />
+                                </div>
+                            </Col>
+                            <Col md={2}>
+                                <h5 className="float-right">
+                                    <span>${price}</span>
+                                </h5>
+                            </Col>
+                        </Row>
+                    </Segment>
+                )
+            )}
         </>
     );
 }
