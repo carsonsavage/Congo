f25db3df1ab6e530c5529c2ea6720668f6ee02b7
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-08 00:41:47 -0600

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { CookiesProvider } from "react-cookie";
+import StateController from "./StateController.js";
+
 
 ReactDOM.render(
     <React.StrictMode>
         <CookiesProvider>
-            <App />
+            <StateController>
+                <App />
+            </StateController>
         </CookiesProvider>
     </React.StrictMode>,
     document.getElementById("root")
