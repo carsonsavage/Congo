88c8d7867eaa55d2dc27084cf2c14ba0e8a0b2c5
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-09 12:38:53 -0600

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -12,7 +12,6 @@ passport.use(
             usernameField: "email",
         },
         function (email, password, done) {
-
             db.User.findOne(
                 {
                     email: email,
@@ -23,15 +22,18 @@ passport.use(
                         return done(null, false, {
                             message: "Incorrect email.",
                         });
-                    }
-                    // If there is a user with the given email, but the password the user gives us is incorrect
-                    else if (passwordCheck) {
-                        return done(null, false, {
-                            message: "Incorrect password.",
+                    } else {
+                        bcrypt.compare(password, dbUser.password, (err, result) => {
+                            // If there is a user with the given email, but the password the user gives us is incorrect
+                            if (!result) {
+                                return done(null, false, {
+                                    message: "Incorrect password.",
+                                });
+                            }
+                            // If none of the above, return the user
+                            return done(null, dbUser);
                         });
                     }
-                    // If none of the above, return the user
-                    return done(null, dbUser);
                 }
             );
         }
