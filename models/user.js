ca293f843483484fe4e7e9446cb98e5be96553ff
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-08 23:38:59 -0600

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    first_name: String,
-    last_name: String,
-    email: String,
+    first_name: { type: String, trim: true },
+    last_name: { type: String, trim: true },
+    email: { type: String, lowercase: true, trim: true },
     password: String,
-    phone_num: String,
+    phone_num: { type: String, trim: true },
     address: Array,
     credit_cards: Array,
     date_Updated: { type: Date, default: Date.now }
