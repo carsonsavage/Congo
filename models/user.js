74c5db5c27d77d3ac1314a35ac40d692896bc14d
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-09 08:56:53 -0600

    Update user.js

--- models/user.js
+++ models/user.js
@@ -6,11 +6,27 @@ const userSchema = new mongoose.Schema({
     email: { type: String, lowercase: true, trim: true },
     password: String,
     phone_num: { type: String, trim: true },
-    address: Array,
-    credit_cards: Array,
-    date_Updated: { type: Date, default: Date.now }
+    address: [
+        {
+            name: { type: String, uppercase: true },
+            address1: { type: String, uppercase: true },
+            address2: { type: String, uppercase: true },
+            city: { type: String, uppercase: true },
+            state: { type: String, uppercase: true },
+            zipcode: { type: String, uppercase: true }
+        },
+    ],
+    credit_cards: [
+        {
+            card_number: { type: String, uppercase: true },
+            card_name: { type: String, uppercase: true },
+            expire_month: { type: String, uppercase: true },
+            expire_year: { type: String, uppercase: true }
+        },
+    ],
+    date_Updated: { type: Date, default: Date.now },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
