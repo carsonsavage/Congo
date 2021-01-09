bcc3c6c6cb5ded2548053e3118876e316458b846
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-08 19:13:35 -0600

    Update signup.js

--- client/src/pages/signup/signup.js
+++ client/src/pages/signup/signup.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import Wrapper from '../../components/wrapper/wrapper';
-import Logo from '../../components/loginPage/logo/logo.js';
-import SignupForm from '../../components/signupPage/signup-form/signup-form.js';
+import React from "react";
+import Wrapper from "../../components/wrapper/wrapper";
+import Logo from "../../components/loginPage/logo/logo.js";
+import SignupForm from "../../components/signupPage/signup-form/signup-form.js";
+import "./signup.css";
 
-function Signup(){
+function Signup() {
     return (
         <>
-        <Wrapper>
-            <Logo />
-            <SignupForm />
-        </Wrapper>
+            <Wrapper>
+                <div id="signup-page">
+                    <Logo />
+                    <SignupForm />
+                </div>
+            </Wrapper>
         </>
-    )
-};
+    );
+}
 
 export default Signup;
