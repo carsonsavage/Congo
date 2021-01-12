b67c5ab00092a98dc437d38a6d41ffb6cad4674b
Teshera Lee <tcasey0825@gmail.com>
2021-01-11 22:54:52 -0500

    Contact Form
    Created Contact Form

---
+++ client/src/pages/contact-us/contact-us.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon, MDBInput } from 'mdbreact';
+import "./contact-us.css"
+import Button from "react-bootstrap/Button";
+
+function ContactUs() {
+    return (
+
+        <>
+            <MDBContainer id="contactus-form">
+                <MDBRow className="d-flex justify-content-center">
+                    <MDBCol md="6">
+                        <form>
+                            <p className="h5 text-center mb-4">Write to us</p>
+                            <div className="grey-text">
+                                <MDBInput label="Your name" icon="user" group type="text" validate error="wrong"
+                                    success="right" />
+                                <MDBInput label="Your email" icon="envelope" group type="email" validate error="wrong"
+                                    success="right" />
+                                <MDBInput label="Subject" icon="tag" group type="text" validate error="wrong" success="right" />
+                                <MDBInput type="textarea" rows="2" label="Your message" icon="pencil-alt" />
+                            </div>
+                            <div className="text-center">
+                                <Button
+                                    block
+                                    size="lg"
+                                    type="submit"
+                                    color= "green"
+                                >
+                                    Send
+                </Button>
+                            </div>
+                        </form>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+        </>
+    )
+}
+
+export default ContactUs
