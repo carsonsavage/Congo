184a280be46f9c5091ffb7a3cda63903141d911b
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-13 13:23:40 -0600

    Update forgot-password-form.js

--- client/src/components/forgotPasswordPage/forgot-password-form.js
+++ client/src/components/forgotPasswordPage/forgot-password-form.js
@@ -9,10 +9,19 @@ import {
 } from "semantic-ui-react";
 import "./forgot-password-form.css";
 import { Link } from "react-router-dom";
+import API from "../../util/API.js";
 
 function ForgotPasswordForm() {
     const [messageState, setMessageState] = useState("");
-    
+    const [email, setEmail] = useState("");
+
+    function sendPasswordReset(event) {
+        event.preventDefault();
+        API.checkUserEmail(email).then(({ data }) => {
+            console.log(data);
+        });
+    }
+
     return (
         <div id="forgotten-password-form">
             <Header as="h2" icon textAlign="center">
@@ -24,24 +33,34 @@ function ForgotPasswordForm() {
                 <Form inverted>
                     <Form.Field>
                         <label>Email</label>
-                        <input placeholder="your@forgotten-email.com" />
+                        <input
+                            type="email"
+                            placeholder="your@forgotten-email.com"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                     </Form.Field>
-                    <Button type="submit">Submit</Button>
+                    <Button type="submit" onClick={sendPasswordReset}>
+                        Submit
+                    </Button>
                 </Form>
             </Segment>
 
-            <Message success>
-                <Message.Header id="reset-password-message">
-                    Check your email for a reset link
-                </Message.Header>
-            </Message>
+            {messageState === "success" && (
+                <Message success>
+                    <Message.Header id="reset-password-message">
+                        Check your email for a reset link
+                    </Message.Header>
+                </Message>
+            )}
 
-            <Message negative>
-                <Message.Header id="reset-password-message">
-                    Sorry, we couldn't find that email. Try again or you can{" "}
-                    <Link to="/contact-us"> Contact Us</Link>
-                </Message.Header>
-            </Message>
+            {messageState === "negative" && (
+                <Message negative>
+                    <Message.Header id="reset-password-message">
+                        Sorry, we couldn't find that email. Try again or you can{" "}
+                        <Link to="/contact-us"> Contact Us</Link>
+                    </Message.Header>
+                </Message>
+            )}
         </div>
     );
 }
