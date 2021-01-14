b75e7b09efbe90409f97cc9e0103e7cfb2570c4c
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-13 18:38:16 -0600

    Update index.js

--- models/index.js
+++ models/index.js
@@ -2,6 +2,6 @@ module.exports = {
     User: require("./user"),
     Product: require("./products"),
     Order: require("./orders"),
-    Cart: require("./cart.js")
-  };
-  
+    Cart: require("./cart.js"),
+    ResetPassword: require("./reset-password.js"),
+};
