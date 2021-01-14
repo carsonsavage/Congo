8ebc32ed519d908b41e9fefa578684e23117ccf2
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-13 21:25:19 -0600

    Update resetPasswordController.js

--- controllers/resetPasswordController.js
+++ controllers/resetPasswordController.js
@@ -2,8 +2,8 @@ const db = require("../models");
 
 // Defining methods for the usersController
 module.exports = {
-    findUserResetAndDelete: function ({req}, res) {
-        db.Cart.findOneAndDelete({ user_id: req.params.id })
+    findUserResetAndDelete: function (req, res) {
+        db.ResetPassword.findOneAndDelete({ user_id: req.params.id })
             .then((dbModel) => res.json(dbModel))
             .catch((err) => res.status(422).json(err));
     },
