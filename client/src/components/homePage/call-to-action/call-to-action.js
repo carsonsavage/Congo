329a0737fe645d21191985151e31c0cdc5a249d2
Carson Savage <carsonesavage@gmail.com>
2021-01-16 09:50:13 -0600

    call to action

--- client/src/components/homePage/call-to-action/call-to-action.js
+++ client/src/components/homePage/call-to-action/call-to-action.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import './call-to-action.css';
+import { Card } from "react-bootstrap";
+import BootstrapBtn from "react-bootstrap/Button";
 
-function CallToAction(){
-    return (
-        <div className="call-to-action">
-            <h3>call to action</h3>
-        </div>
-    )
-};
+const CardExample = () => {
+return (
+    <Card className="text-center">
+    <Card.Body className='card-image'
+        style = {
+            {
+                backgroundImage: "url('https://i.imgur.com/f4W2cLs.png')"
+            }
+        } >
+    <Card.Title style = {
+        {
+            color: 'white'
+        }
+    }><b><em>Ready to get started?</em></b></Card.Title>
+    < Card.Text 
+    style = {
+        {
+            color: 'white'
+        }
+    }
+    className = "mb-2" >
+        Sign up here!
+    </Card.Text>
+    <BootstrapBtn block size="lg" type="submit" id="signup-btn" variant="success">
+        Sign up
+    </BootstrapBtn>
+</Card.Body>
+</Card>
+)
+}
 
-export default CallToAction;
+export default CardExample;
