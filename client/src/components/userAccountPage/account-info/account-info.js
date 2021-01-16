9a360bce7c283a254ecde044e550524d67b3bb0a
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-16 11:29:04 -0600

    Update account-info.js

--- client/src/components/userAccountPage/account-info/account-info.js
+++ client/src/components/userAccountPage/account-info/account-info.js
@@ -1,8 +1,29 @@
 import React, { useContext, useState } from "react";
 import "./account-info.css";
 import UserContext from "../../../util/userContext.js";
+import { Modal, Button } from "semantic-ui-react";
+import ChangePasswordModal from "../change-password-modal/change-password-modal.js";
 
 function AccountInfo() {
+    function exampleReducer(state, action) {
+        switch (action.type) {
+            case "OPEN_MODAL":
+                return { open: true, dimmer: action.dimmer };
+            case "CLOSE_MODAL":
+                return { open: false };
+            default:
+                throw new Error();
+        }
+    }
+
+    const [state, dispatch] = React.useReducer(exampleReducer, {
+        open: false,
+        dimmer: undefined,
+    });
+    const { open, dimmer } = state;
+
+    const [modalState, setModalState] = useState("");
+
     const {
         editableUserState,
         handleUserInfoChange,
@@ -10,6 +31,8 @@ function AccountInfo() {
     } = useContext(UserContext);
 
     const [editState, setEditState] = useState(false);
+
+    const [password, setPassword] = useState();
 
     function enableEdit() {
         setEditState(true);
@@ -21,7 +44,7 @@ function AccountInfo() {
     }
 
     return (
-        <div class="ui raised segment">
+        <div class="ui raised segment clearfix">
             <button class="ui red ribbon label">Overview</button>
             <h4>Account Details</h4>
             {editState ? (
@@ -83,6 +106,19 @@ function AccountInfo() {
                     >
                         <i class="edit icon"></i>
                     </button>
+                    <button
+                        className="ui icon button mini float-right"
+                        id="change-password-btn"
+                        onClick={() => {
+                            setModalState("password");
+                            dispatch({
+                                type: "OPEN_MODAL",
+                                dimmer: "blurring",
+                            });
+                        }}
+                    >
+                        Change Password
+                    </button>
                     <hr />
                     <p>
                         <span>First Name:</span> {editableUserState.first_name}
@@ -98,6 +134,21 @@ function AccountInfo() {
                     </p>
                 </>
             )}
+            <Modal
+                dimmer={dimmer}
+                open={open}
+                onClose={() => dispatch({ type: "CLOSE_MODAL" })}
+                id="card-modal"
+            >
+                {modalState === "password" && (
+                    <ChangePasswordModal dispatch={dispatch} password,
+                    confirmPassword,
+                    oldPassword,
+                    setPassword,
+                    setConfirmPassword,
+                    setOldPassword,/>
+                )}
+            </Modal>
         </div>
     );
 }
