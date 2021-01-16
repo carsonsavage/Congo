86f4554f183a181c3e759fe8ca4ff3ba4f502fe3
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-15 23:32:27 -0600

    Update htmlGenerator.js

--- email-html-generator/htmlGenerator.js
+++ email-html-generator/htmlGenerator.js
@@ -234,7 +234,7 @@ const EmailHTML = {
                                                                     style="
                                                                         border-radius: 3px;
                                                                     "
-                                                                    bgcolor="#4A35EA"
+                                                                    bgcolor="green"
                                                                 >
                                                                     <a
                                                                         href="http://localhost:3000/user/reset-password/${user_id}"
@@ -253,7 +253,7 @@ const EmailHTML = {
                                                                             border-radius: 2px;
                                                                             border: 1px
                                                                                 solid
-                                                                                #4a35ea;
+                                                                                green;
                                                                             display: inline-block;
                                                                         "
                                                                         >Reset Password</a
@@ -306,7 +306,7 @@ const EmailHTML = {
                                                 <a
                                                     href="http://localhost:3000/user/reset-password/${user_id}"
                                                     target="_blank"
-                                                    style="color: #4a35ea"
+                                                    style="color: green"
                                                     >http://localhost:3000/user/reset-password/${user_id}</a
                                                 >
                                             </p>
@@ -463,6 +463,213 @@ const EmailHTML = {
                 </body>
             </html>
             `);
+        });
+    },
+
+    orderConfirm: (order_num, item_count, preTax, shippingHandling, tax, order_total, address, delivery_date) => {
+        return new Promise((resolve, reject) => {
+            resolve(`<!DOCTYPE html>
+            <html>
+            
+            <head>
+                <title></title>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <style type="text/css">
+                    body,
+                    table,
+                    td,
+                    a {
+                        -webkit-text-size-adjust: 100%;
+                        -ms-text-size-adjust: 100%;
+                    }
+            
+                    body {
+                        height: 100% !important;
+                        margin: 0 !important;
+                        padding: 0 !important;
+                        width: 100% !important;
+                    }
+            
+                    a[x-apple-data-detectors] {
+                        color: inherit !important;
+                        text-decoration: none !important;
+                        font-size: inherit !important;
+                        font-family: inherit !important;
+                        font-weight: inherit !important;
+                        line-height: inherit !important;
+                    }
+            
+                    @media screen and (max-width: 480px) {
+                        .mobile-hide {
+                            display: none !important;
+                        }
+            
+                        .mobile-center {
+                            text-align: center !important;
+                        }
+                    }
+            
+                    div[style*="margin: 16px 0;"] {
+                        margin: 0 !important;
+                    }
+                </style>
+            
+            <body style="margin: 0 !important; padding: 0 !important; background-color: #eeeeee;" bgcolor="#080808">
+                
+                <table border="0" cellpadding="0" cellspacing="0" width="100%" >
+                    <tr>
+                        <td align="center" style="background-color: #fcf7f7;" bgcolor="#080808">
+                            <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600px;">
+                                <tr>
+                                    <td align="center" valign="top" style="font-size:0; padding: 35px; background-color: green;" bgcolor="#0e0d0d">
+                                        <div style="display:inline-block; max-width:50%; min-width:100px; vertical-align:top; width:100%;">
+                                            <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:300px;">
+                                                </tr>
+                                            </table>
+                                        </div>
+                                        <div style="display:inline-block; max-width:50%; min-width:100px; vertical-align:top; width:100%;" class="mobile-hide">
+                                            <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:300px;">
+                                                <tr>
+                                                    <td align="right" valign="top" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; line-height: 48px;">
+                                                        <table cellspacing="0" cellpadding="0" border="0" align="right">
+                                                            <tr>
+                                                                <td style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 35px; font-weight: 800;">
+                                                                    <p style="font-size: 30px; font-weight: 400; margin: 0; color: #ffffff;"><a href="" target="_blank" style="color: #ffffff; text-decoration: none;"> Order Confirmation </a></p>
+                                                                </td>
+                                                                <td style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 24px;"> <a href="#" target="_blank" style="color: #0c0c0c; text-decoration: none;">
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td align="center" style="padding: 35px 35px 20px 35px; background-color: #ffffff;" bgcolor="#ffffff">
+                                        <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600px;">
+                                            <tr>
+                                                <td align="center" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding-top: 25px;"> <img src="https://img.icons8.com/carbon-copy/100/000000/checked-checkbox.png" width="125" height="120" style="display: block; border: 0px;" /><br>
+                                                    <h2 style="font-size: 30px; font-weight: 800; line-height: 36px; color: green; margin: 0;"> Thank You For Your Order! </h2>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding-top: 10px;">
+                                                    
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="left" style="padding-top: 20px;">
+                                                    <table cellspacing="0" cellpadding="0" border="0" width="100%">
+                                                        <tr>
+                                                            <td width="75%" align="left" bgcolor="#eeeeee" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px;"> Order Confirmation # </td>
+                                                            <td width="25%" align="left" bgcolor="#eeeeee" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px;"> ${order_num} </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td width="75%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 15px 10px 5px 10px;"> Purchased Item (${item_count}) </td>
+                                                            <td width="25%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 15px 10px 5px 10px;"> $${preTax} </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td width="75%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 5px 10px;"> Shipping + Handling </td>
+                                                            <td width="25%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 5px 10px;"> $${shippingHandling} </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td width="75%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 5px 10px;"> Sales Tax </td>
+                                                            <td width="25%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding: 5px 10px;"> $${tax} </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="left" style="padding-top: 20px;">
+                                                    <table cellspacing="0" cellpadding="0" border="0" width="100%">
+                                                        <tr>
+                                                            <td width="75%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px; border-top: 3px solid #eeeeee; border-bottom: 3px solid #eeeeee;"> TOTAL </td>
+                                                            <td width="25%" align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 800; line-height: 24px; padding: 10px; border-top: 3px solid #eeeeee; border-bottom: 3px solid #eeeeee;"> $${order_total} </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td align="center" height="100%" valign="top" width="100%" style="padding: 0 35px 35px 35px; background-color: #ffffff;" bgcolor="#ffffff">
+                                        <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:660px;">
+                                            <tr>
+                                                <td align="center" valign="top" style="font-size:0;">
+                                                    <div style="display:inline-block; max-width:50%; min-width:240px; vertical-align:top; width:100%;">
+                                                        <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:300px;">
+                                                            <tr>
+                                                                <td align="left" valign="top" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px;">
+                                                                    <p style="font-weight: 800;">Delivery Address</p>
+                                                                    <p>${address.address1} ${address.address2} <br> ${address.city} , ${address.state} <br> ${address.zipcode} </p>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                    </div>
+                                                    <div style="display:inline-block; max-width:50%; min-width:240px; vertical-align:top; width:100%;">
+                                                        <table align="left" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:300px;">
+                                                            <tr>
+                                                                <td align="left" valign="top" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px;">
+                                                                    <p style="font-weight: 800;">Estimated Delivery Date</p>
+                                                                    <p>${delivery_date}</p>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td align="center" style=" padding: 35px; background-color:#005300;" bgcolor="#1b9ba3">
+                                        <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600px;">
+                                            <tr>
+                                                <td align="center" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 400; line-height: 24px; padding-top: 25px;">
+                                                    <h2 style="font-size: 24px; font-weight: 800; line-height: 30px; color: #ffffff; margin: 0;"> Get 30% off your next order. </h2>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="center" style="padding: 25px 0 15px 0;">
+                                                    <table border="0" cellspacing="0" cellpadding="0">
+                                                        <tr>
+                                                            <td align="center" style="border-radius: 5px;" bgcolor="#66b3b7"> <a href="http://localhost:3000/" target="_blank" style="font-size: 18px; font-family: Open Sans, Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; border-radius: 5px; background-color: green; padding: 15px 30px; border: 1px solid #f0e6e6; display: block;">Shop Again</a> </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td align="center" style="padding: 35px; background-color: #ffffff;" bgcolor="#ffffff">
+                                        <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width:600px;">
+                                            <tr>
+                                                <td align="center">  </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="center" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px; padding: 5px 0 10px 0;">
+                                                    <p style="font-size: 14px; font-weight: 800; line-height: 18px; color: #f7f7f7;">  <p>410 Terry Ave WA <br> North Seattle, <br>Cambridge, 98109-5210</p>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="left" style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 24px;">
+                                                    <p style="font-size: 14px; font-weight: 400; line-height: 20px; color: #777777;"> If you didn't create an account using this email address, please ignore this email or <a href="#" target="_blank" style="color: #777777;">unsusbscribe</a>. </p>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </body>
+            
+            </html>`);
         });
     },
 };
