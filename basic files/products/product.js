de2138f7e4bb3adce0a01155c4aff87ea5be7efa
Teshera Lee <tcasey0825@gmail.com>
2021-01-16 14:16:51 -0500

    updated products
    added books catagory to products.

--- basic files/products/product.js
+++ basic files/products/product.js
@@ -19,21 +19,24 @@ function readWriteFile(product) {
 
 let product = {
     "uid": translator.new(),
-    "title": "Llama Llama I Love You Board book – Illustrated, December 26, 2014",
-    "price": 5.00,
-    "quantity": 12,
+    "title": "Do It Afraid: Embracing Courage in the Face of Fear Hardcover – September 1, 2020",
+    "price": 17.99,
+    "quantity": 30,
     "category": "Books",
     "keywords": [
-        "Llama book",
-        "reading",
-        "kids book"
+        "get over fears",
+        "conquer the fears holding you back"
     ],
     "features": [
-        "story about llamas",
-        "kids i love you book"
+        "Recognize, confront, and conquer the fears",
     ],
-    "description": "With short and simple rhyming text, the Llama Llama board books introduce Llama Llama to babies and toddlers before they’re ready for longer full-length stories. And their small size and durable pages are perfect for little hands.",
-    "images": ["https://images-na.ssl-images-amazon.com/images/I/91xQR0hElJL.jpg", "https://images-na.ssl-images-amazon.com/images/I/916ha2D7y4L.jpg"]
+    "description": "Recognize, confront, and conquer the fears holding you back from living boldly and freely with renowned Bible teacher and New York Times bestselling author, Joyce Meyer. Fear is the devil's favorite tool in the toolbox of schemes he uses to destroy God's good plan for you. He uses it to hold you back and prevent progress in your relationships, career, and more. In Do It Afraid, Joyce Meyer explains that fear is everywhere and affects everyone. It rules many people, but it doesn't have to rule you any longer. She will teach you how to: Understand fear and recognize how it works in your life. Confront those fears that are holding you back. Change your mindset for lasting freedom from some of the most common fears people face. Remember, courage isn't the absence of fear; it is learning how to move forward in the presence of fear. Courageous people do what they believe in their hearts they should do, no matter how they feel or what doubts fill their minds. When you take ownership of your problems and open your heart to God, He will help bring light into darkness so that you can be free.",
+    "images": [
+        "https://images-na.ssl-images-amazon.com/images/I/91Xlq4vS6FL.jpg",
+        "https://images-na.ssl-images-amazon.com/images/I/918Cnh7hQYL.jpg",
+        "https://images-na.ssl-images-amazon.com/images/I/91nHmHBSPVL.jpg",
+        "https://images-na.ssl-images-amazon.com/images/I/718AxUutTrL.jpg"
+    ]
 };
 
 product.category = product.category.toLowerCase();
