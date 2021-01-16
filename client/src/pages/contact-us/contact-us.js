b48015afbe2308b964e27679882d7f8349c23716
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-15 19:34:50 -0600

    Update contact-us.js

--- client/src/pages/contact-us/contact-us.js
+++ client/src/pages/contact-us/contact-us.js
@@ -1,50 +1,62 @@
-import React, { Fragment, Component } from "react";
+import React, { Fragment, useState } from "react";
 import { MDBContainer, MDBRow, MDBCol, MDBInput } from "mdbreact";
 import "./contact-us.css";
 import Button from "react-bootstrap/Button";
 
 function ContactUs() {
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [subject, setSubject] = useState();
+    const [message, setMessage] = useState();
+
     return (
         <>
             <MDBContainer id="contactus-form">
                 <MDBRow className="d-flex justify-content-center">
                     <MDBCol md="6">
                         <form>
-                            <p className="h5 text-center mb-4">Write to us</p>
+                            <div className="orders-header center">
+                                <h1 className="ui icon header">
+                                    <i className="envelope open outline icon"></i>
+                                    <div className="content">Write to us</div>
+                                </h1>
+                            </div>
+                            <hr />
                             <div className="grey-text">
                                 <label>Your name</label>
-                                <MDBInput
-                                    icon="user"
-                                    type="text"
-                                    validate
-                                    error="wrong"
-                                    success="right"
-                                />
+                                <MDBInput type="text" />
                                 <label>Your email</label>
-                                <MDBInput
-                                    icon="envelope"
-                                    type="email"
-                                    validate
-                                    error="wrong"
-                                    success="right"
-                                />
+                                <MDBInput type="email" />
                                 <label>Subject</label>
                                 <select
                                     className="browser-default custom-select"
                                     id="contact-us-select"
                                     group
                                 >
-                                    <option>Choose your option</option>
-                                    <option value="1">Option 1</option>
-                                    <option value="2">Option 2</option>
-                                    <option value="3">Option 3</option>
+                                    <option>Choose your option...</option>
+                                    <option value="Product Inquiry">
+                                        Product Inquiry
+                                    </option>
+                                    <option value="Sales">Sales</option>
+                                    <option value="Sales Support">
+                                        Sales Support
+                                    </option>
+                                    <option value="Account Issues">
+                                        Account Issues
+                                    </option>
+                                    <option value="Order Issues/Questions">
+                                        Order Issues/Questions
+                                    </option>
+                                    <option value="Other">Other</option>
                                 </select>
+                                {subject === "Other" && (
+                                    <>
+                                        <label>Custom subject</label>
+                                        <MDBInput type="text" />
+                                    </>
+                                )}
                                 <label>Your Message</label>
-                                <MDBInput
-                                    type="textarea"
-                                    rows="2"
-                                    icon="pencil-alt"
-                                />
+                                <MDBInput type="textarea" rows="2" />
                             </div>
                             <div className="text-center">
                                 <Button
