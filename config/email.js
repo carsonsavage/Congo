422f4213e493e9e90be5559df1e02c6e91b295ef
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-15 22:07:17 -0600

    Update email.js

--- config/email.js
+++ config/email.js
@@ -1,10 +1,15 @@
 const HTMLGEN = require("../email-html-generator/htmlGenerator.js");
+const NodeMailer = require("./nodeMailer.js");
 
 const Emailer = {
     generateContactUsEmail: (emailObj) => {
         //do something here
         HTMLGEN.contactUs(emailObj).then((html) => {
-            console.log(html);
+            NodeMailer.sendEmail(
+                `**[${emailObj.subject}]** - webpage contact form`,
+                "user.support@congo-marketplace.com",
+                html
+            );
         });
     },
 };
