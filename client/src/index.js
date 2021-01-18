17d51c6336f18f8a3351d075837bc86d50ae41c0
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-18 16:46:00 -0600

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { CookiesProvider } from "react-cookie";
-import StateController from "./StateController.js";
+import CartController from "./contextControllers/cart-controller.js";
+import SearchController from "./contextControllers/search-controller.js";
+import UserController from "./contextControllers/user-controller.js";
+import OrderController from "./contextControllers/order-controller.js";
 
 ReactDOM.render(
     <CookiesProvider>
-        <StateController>
-            <App />
-        </StateController>
+        <CartController>
+            <SearchController>
+                <UserController>
+                    <OrderController>
+                        <App />
+                    </OrderController>
+                </UserController>
+            </SearchController>
+        </CartController>
     </CookiesProvider>,
     document.getElementById("root")
 );
