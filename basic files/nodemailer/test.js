ac79806fe7caec667e3814b6902214497dcd43bf
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-01-19 10:57:49 -0600

    Delete test.js

--- basic files/nodemailer/test.js
+++
@@ -1,4 +0,0 @@
-const mailer = require("./mailer.js");
-
-//how to call an email to send to the etherreal email for testing. First param is subject, second is the HTML Body to send
-mailer.sendEmail("Test Subject", `<h1>Hola senor</h1>`);
