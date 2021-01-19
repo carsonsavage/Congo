a13c2767757b42079de9b92af348cea3ae852ceb
Carson Savage <carsonesavage@gmail.com>
2021-01-18 21:19:48 -0600

    Update footer.js

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -3,46 +3,46 @@ import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 import "./footer.css";
 
 const FooterPage = () => {
-  return (
+return (
     <MDBFooter color="dark grey" className="font-small pt-4 mt-4" id="footer">
-      <MDBContainer fluid className="text-center text-md-left">
+        <MDBContainer fluid className="text-center text-md-left">
         <MDBRow>
-          <MDBCol md="4">
+            <MDBCol md="4">
             <h5 className="title">Congo</h5>
             <p>
-              Making shopping a breeze.
+                Making shopping a breeze.
             </p>
-          </MDBCol>
-          <MDBCol md="4">
+            </MDBCol>
+            <MDBCol md="4">
             <h5 className="title">Issues?</h5>
             <a href="#">Contact us here</a>
-          </MDBCol>
-          <MDBCol md="4">
+        </MDBCol>
+        <MDBCol md="4">
             <h5 className="title">Links to our Github Pages:</h5>
             <ul>
-              <li className="list-unstyled">
-                <a href="https://github.com/DaySloth">Allister</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="https://github.com/carsonsavage">Carson</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="https://github.com/beshayr2020">Beshayr</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="https://github.com/tlee0825">Teshera</a>
-              </li>
+                <li className="list-unstyled">
+                    <a href="https://github.com/DaySloth">Allister</a>
+                </li>
+                <li className="list-unstyled">
+                    <a href="https://github.com/carsonsavage">Carson</a>
+                </li>
+                <li className="list-unstyled">
+                    <a href="https://github.com/beshayr2020">Beshayr</a>
+                </li>
+                <li className="list-unstyled">
+                    <a href="https://github.com/tlee0825">Teshera</a>
+                </li>
             </ul>
-          </MDBCol>
+        </MDBCol>
         </MDBRow>
-      </MDBContainer>
-      <div className="footer-copyright text-center py-3">
+    </MDBContainer>
+    <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright: <a href="https://github.com/DaySloth/Project3"> Congo </a>
+        &copy; {new Date().getFullYear()} Copyright: <a href="https://github.com/DaySloth/Project3"> Congo </a>
         </MDBContainer>
-      </div>
+    </div>
     </MDBFooter>
-  );
+);
 }
 
 export default FooterPage;
